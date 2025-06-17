cca8bb18f1e702d42ca667b735cb2ba5d2c4958a
Jordan Grieve <grievejordan@yahoo.com>
2025-06-17 19:42:45 +0100

    creation of folders

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+      <h1>VIte Treact APp</h1>
+      <h2>rererr</h2>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,5 @@
+function youTube() {
+  return <h1>YoutUBE app</h1>;
+}
+
+export default youTube;
