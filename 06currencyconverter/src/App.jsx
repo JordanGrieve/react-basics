0465bd4f731740c16699f3c5edf15deb69292bcf
Jordan Grieve <grievejordan@yahoo.com>
2025-06-24 17:05:37 +0100

    Creation of the currency project and inputBox Component

---
+++ 06currencyconverter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url(https://images.pexels.com/photos/8369648/pexels-photo-8369648.jpeg)",
+      }}
+    >
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
