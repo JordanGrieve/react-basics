c1caaa402a608f86871381915b599fe6a333cd23
Jordan Grieve <grievejordan@yahoo.com>
2025-06-26 12:44:55 +0100

    Creation of the my 7th mini react project - Router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 p-5 py-2 rounded">HRWHERER</h1>
+    </>
+  );
+}
+
+export default App;
