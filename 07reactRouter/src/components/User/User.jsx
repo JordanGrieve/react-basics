68fe90e4507a1c01702a6b0a80d9f9b9231ac280
Jordan Grieve <grievejordan@yahoo.com>
2025-06-26 15:44:17 +0100

    Added the user page and a user variable with a route of :userid

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { use } from "react";
+import { useParams } from "react-router-dom";
 
 function User() {
+  const { userid } = useParams();
   return (
     <div className="py-5 bg-orange-500 text-black text-3xl text-center">
-      User
+      {"user " ? userid : "not found"}
     </div>
   );
 }
