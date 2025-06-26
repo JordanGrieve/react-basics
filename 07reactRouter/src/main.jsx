4607a795a52e6ffb20733a3491d12a83532da033
Jordan Grieve <grievejordan@yahoo.com>
2025-06-26 15:11:02 +0100

    Header and navbar created

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import ReactDom from "react-dom/client";
 import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
 
 import {
   createBrowserRouter,
@@ -13,7 +15,12 @@ import {
 } from "react-router-dom";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Layout />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
