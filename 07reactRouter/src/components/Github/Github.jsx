50414b4d4816195f26bcca0cd2c2e290696f239a
Jordan Grieve <grievejordan@yahoo.com>
2025-06-26 16:28:27 +0100

    Added useLoaderData to make getting data more efficient

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = React.useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/JordanGrieve").then((response) =>
-      response.json().then((data) => {
-        setData(data);
-        console.log(data);
-      })
-    );
-  }, []);
+  const data = useLoaderData();
+
+  //   const [data, setData] = React.useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/JordanGrieve").then((response) =>
+  //       response.json().then((data) => {
+  //         setData(data);
+  //         console.log(data);
+  //       })
+  //     );
+  //   }, []);
+
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github followers: {data.followers} <br />
@@ -24,3 +28,8 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/JordanGrieve");
+  return response.json();
+};
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -7,7 +7,7 @@ import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import User from "./components/User/User.jsx";
-import Github from "./components/Github/Github.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 import {
   createBrowserRouter,
@@ -24,7 +24,7 @@ const router = createBrowserRouter(
       <Route path="user/" element={<User />}>
         <Route path=":userid" element={<User />} />
       </Route>
-      <Route path="github" element={<Github />} />
+      <Route path="github" element={<Github />} loader={githubInfoLoader} />
       <Route path="*" element={<h1>Not Found</h1>} />
     </Route>
   )
